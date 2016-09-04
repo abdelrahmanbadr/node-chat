6ae7608d643e8d646ea44124e973ec2c29f6a9c9
abdelrahman <abdelrahmanbadr>
2016-09-04 12:50:09 +0300

    first commit

--- server.js
+++ server.js
@@ -7,17 +7,47 @@ var server = require('http').createServer(app);
 
 var io=require('socket.io').listen(server);
 
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/myapp',function(err){
+  if(err){
+    console.log(err);
+  }
+  else{
+    console.log("connected to db");
+  }
+});
+
+
  usernames=[];
 
 server.listen(3000);
 
+//mongodb part
+///////////////////////////////////
+var the_schema=mongoose.Schema;
 
+var chat_schema=new the_schema({
+user:String,
+msg:String,
+created:{type:Date ,default:Date.now}
+});
+
+chat_model=mongoose.model('chat',chat_schema);
+
+////////////////////////////////////
 app.get('/', function(req, res) {
+
   res.sendFile( __dirname + "/index.html" );
 });
 
 io.sockets.on('connection', function (socket) {
-
+chat_model.find({}, function(err, data){
+		if(err) {res.json(err);}
+		   
+   else  { io.sockets.emit('old_msg',data); }
+    
+	});
  socket.on('username',function(data){
 
    socket.username=data;
@@ -26,8 +56,17 @@ io.sockets.on('connection', function (socket) {
 
  });
   socket.on('msg',function(data){
-    
+
 io.sockets.emit('new_msg',{msg:data,user:socket.username});
+    new chat_model({
+      user:socket.username,
+      msg:data
+
+    }).save(function(err)
+{
+  if(err)  res.json(err); 
+  
+});
  
 
   });
